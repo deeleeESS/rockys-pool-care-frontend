9e7c273ea724f6c443f558984b507e681f66ceef
deeleeESS <dlee@essrocks.io>
2023-04-14 13:51:13 -0500

    added new template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "amber-50": "#FFF9EB",
+        "amber-100": "#FEF6E1",
+        vanilla: "#FFFEF8",
+        "purple-light": "#F3F2F5",
+        "purple-dark": "#A59EB2",
+        "gray-secondary": {
+          50: "#f7f7f5",
+          100: "#edece7",
+          200: "#dbe0e2",
+          300: "#c2ccce",
+          400: "#9eadb2",
+        },
+      },
+      fontSize: {
+        md: "15px",
+      },
+      lineHeight: {
+        tighter: "1.15",
+      },
+
+      keyframes: {
+        infiniteScroll: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+
+      animation: {
+        infiniteScroll: "infiniteScroll 60s linear infinite",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
