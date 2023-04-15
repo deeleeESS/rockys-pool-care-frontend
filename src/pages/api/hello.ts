872f88673de90f567939af6f41d96bcad6c9215f
deeleeESS <dlee@essrocks.io>
2023-04-14 19:34:39 -0500

    added mail file

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { NextApiRequest, NextApiResponse } from "../../../node_modules/next/dist/shared/lib/utils"
+
 
 type Data = {
   name: string
