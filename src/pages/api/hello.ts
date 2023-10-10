c53964dd27d6c0abb31e64f27a6b72419d673a07
deeleeESS <dlee@essrocks.io>
2023-10-10 12:24:37 -0500

    updated api for testing

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,19 +2,41 @@
 
 import { NextApiRequest, NextApiResponse } from "../../../node_modules/next/dist/shared/lib/utils"
 
+const WEBHOOK_API_KEY = process.env.WEBHOOK_API_KEY;
 
 type Data = {
-  name: string
+  message: string
 }
 
-export default function handler(
+function checkAPIKey(req, apiKey) {
+  const providedKey = req.headers['WEBHOOK_API_KEY']; // Assuming the key is sent as "x-api-key"
+  
+  if (!providedKey) {
+      throw new Error('No API key provided.');
+  }
+
+  if (providedKey !== apiKey) {
+      throw new Error('Invalid API key.');
+  }
+}
+
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  try {
+      // Check the API key before proceeding
+      checkAPIKey(req, WEBHOOK_API_KEY);
 
-  if (req.method === 'POST') {
-    console.log('POST data:', req.body);
+      // If the key is valid, the rest of your endpoint logic goes here:
+      if (req.method === 'POST') {
+        console.log('POST data:', req.body);
+      }
+      res.status(200).json({ message: 'Your endpoint logic results.' });
+
+  } catch (error) {
+      // Handle errors thrown by checkAPIKey or any other errors
+      res.status(401).json({ message: error.message }); // 401 for Unauthorized
   }
-
-  res.status(200).json({ name: 'John Doe' })
-}
+};
